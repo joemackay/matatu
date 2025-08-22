be8282f4163e22ca62c3d7365bac641840f6510e
Joe Okatch <joemackay@gmail.com>
2025-08-22 19:09:08 +0300

    remove the login page and make it navigate straight to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import { ArrowRightIcon } from "lucide-react";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Link
-          href="/home"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-        >
-          <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-        </Link>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
-      </footer>
-    </div>
-  );
+  redirect("/home");
 }
